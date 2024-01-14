3aa3b2f391e50a1dd9dee7fa03752868f1bf0ce8
Rayhan Abduhuda <rayrayhan920@gmail.com>
2024-01-14 22:48:11 +0700

    login dan signup aman

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -28,23 +28,27 @@ class _AuthScreenState extends State<AuthScreen> {
 
     _form.currentState!.save();
 
-    if (_isLogin) {
-      // Log user in
-    } else {
-      try {
-        final userCredential = await _firebase.createUserWithEmailAndPassword(
+    try {
+      if (_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
             email: _enteredEmail, password: _enteredPassword);
-        print(userCredential);
-      } on FirebaseAuthException catch (error) {
-        if (error.code == 'email-already-in-use') {}
-        ScaffoldMessenger.of(context).clearSnackBars();
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(error.message ?? 'Autehntication failed!'),
-            // backgroundColor: Theme.of(context).errorColor,
-          ),
-        );
+        print(userCredentials);
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+        print(userCredentials);
       }
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'email-already-in-use') {
+        // ...
+      }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.message ?? 'Autehntication failed!'),
+          // backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
     }
   }
 
