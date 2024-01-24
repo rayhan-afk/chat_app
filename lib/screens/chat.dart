971ed7b1b73be46f062b45b62050999493dc284f
Rayhan Abduhuda <rayrayhan920@gmail.com>
2024-01-24 17:18:21 +0700

    logout

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,17 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-const ChatScreen({Key? key}) : super(key: key);
+  const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('FlutterChat'),
+        title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.logout,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+          ),
+        ],
       ),
-     body: Center(
-       child: Text('Logged in!'),
-     ),
+      body: const Center(
+        child: Text('Logged in!'),
+      ),
     );
   }
 }
